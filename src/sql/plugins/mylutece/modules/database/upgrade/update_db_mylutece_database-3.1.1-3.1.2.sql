dffa3851ce7c0bf567fccb22c73c6caf1eaa6cfd
FrancoisEricMerlin <FrancoisEricMerlin@users.noreply.github.com>
2014-04-08 14:30:29 +0000

    MYLUTECEDATABASE-47: Modify the default email sender name for all notifications
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/authentication/module-mylutece-database@65583 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/sql/plugins/mylutece/modules/database/upgrade/update_db_mylutece_database-3.1.1-3.1.2.sql
@@ -0,0 +1,8 @@
+UPDATE mylutece_database_user_parameter SET parameter_value='LUTECE' WHERE parameter_key = 'expired_alert_mail_sender' ;
+UPDATE mylutece_database_user_parameter SET parameter_value='LUTECE' WHERE parameter_key = 'first_alert_mail_sender' ;
+UPDATE mylutece_database_user_parameter SET parameter_value='LUTECE' WHERE parameter_key = 'other_alert_mail_sender' ;
+UPDATE mylutece_database_user_parameter SET parameter_value='LUTECE' WHERE parameter_key = 'account_reactivated_mail_sender' ;
+UPDATE mylutece_database_user_parameter SET parameter_value='LUTECE' WHERE parameter_key = 'unblock_user_mail_sender' ;
+UPDATE mylutece_database_user_parameter SET parameter_value='LUTECE' WHERE parameter_key = 'password_expired_mail_sender' ;
+UPDATE mylutece_database_user_parameter SET parameter_value='LUTECE' WHERE parameter_key = 'mail_lost_password_sender' ;
+UPDATE mylutece_database_user_parameter SET parameter_value='LUTECE' WHERE parameter_key = 'mail_password_encryption_changed_sender' ;
