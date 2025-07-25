dacde2120a948ffefc3714b19d8ae15797c57d88
evdngsl <158455076+evdngsl@users.noreply.github.com>
2025-07-25 10:40:33 +0200

    LUT-30437 : Prepare SQL scripts for Liquibase plugin

--- src/sql/plugins/mylutece/modules/database/upgrade/update_db_mylutece_database-3.0.2-3.1.1.sql
+++ src/sql/plugins/mylutece/modules/database/upgrade/update_db_mylutece_database-3.0.2-3.1.1.sql
@@ -1,3 +1,6 @@
+--liquibase formatted sql
+--changeset mylutece-database:update_db_mylutece_database-3.0.2-3.1.1.sql
+--preconditions onFail:MARK_RAN onError:WARN
 DELETE FROM mylutece_database_user_parameter WHERE  parameter_key='password_format';
 INSERT INTO mylutece_database_user_parameter VALUES ('password_format_upper_lower_case', 'false');
 INSERT INTO mylutece_database_user_parameter VALUES ('password_format_numero', 'false');
