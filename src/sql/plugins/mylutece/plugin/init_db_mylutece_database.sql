caa940a9b34582b06e30d8a550e7d32b86cc161d
evdngsl <158455076+evdngsl@users.noreply.github.com>
2025-11-25 16:21:58 +0100

    LUT-31272 : Fix init scripts to conform with FileStoreService, Fix liquibase headers

--- src/sql/plugins/mylutece/plugin/init_db_mylutece_database.sql
+++ src/sql/plugins/mylutece/plugin/init_db_mylutece_database.sql
@@ -1,6 +1,6 @@
---liquibase formatted sql
---changeset mylutece-database:init_db_mylutece_database.sql
---preconditions onFail:MARK_RAN onError:WARN
+-- liquibase formatted sql
+-- changeset mylutece-database:init_db_mylutece_database.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 INSERT INTO mylutece_user_anonymize_field (field_name, anonymize) VALUES ('login', 1);
 INSERT INTO mylutece_user_anonymize_field (field_name, anonymize) VALUES ('name_given', 1);
 INSERT INTO mylutece_user_anonymize_field (field_name, anonymize) VALUES ('name_family', 1);
