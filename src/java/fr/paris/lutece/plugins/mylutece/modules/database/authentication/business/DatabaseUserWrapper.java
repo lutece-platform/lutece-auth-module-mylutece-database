f8e313f2a4f7eba96431ae5bc9f00b84ca8f32b2
evdnjde <jeremy.deloche@atos.net>
2026-03-24 16:11:18 +0100

    LUT-32203 : Replace XSL Export user

---
+++ src/java/fr/paris/lutece/plugins/mylutece/modules/database/authentication/business/DatabaseUserWrapper.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright (c) 2002-2025, City of Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.mylutece.modules.database.authentication.business;
+
+import fr.paris.lutece.util.string.StringUtil;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * This class represents the business object DatabaseUserExport
+ */
+public class DatabaseUserWrapper implements Serializable
+{
+
+    private final DatabaseUser _databaseUser;
+
+    private List<String> _roles;
+    private List<String> _groups;
+    private List<String> _attributes;
+
+    public DatabaseUserWrapper(DatabaseUser databaseUser)
+    {
+        _databaseUser = databaseUser;
+    }
+
+    /**
+     * Returns the user access code.
+     *
+     * @return the user access code
+     */
+    public String getAccessCode() {
+        return _databaseUser.getLogin();
+    }
+
+    /**
+     * Returns the user last name.
+     *
+     * @return the user last name
+     */
+    public String getLastName() {
+        return _databaseUser.getLastName( );
+    }
+
+    /**
+     * Returns the user first name.
+     *
+     * @return the user first name
+     */
+    public String getFirstName() {
+        return _databaseUser.getFirstName( );
+    }
+
+    /**
+     * Returns the user email address.
+     *
+     * @return the user email address
+     */
+    public String getEmail() {
+        return _databaseUser.getEmail( );
+    }
+
+    /**
+     * Returns the user status.
+     *
+     * @return the user status
+     */
+    public int getStatus() {
+        return _databaseUser.getStatus( );
+    }
+
+    /**
+     * Returns the maximum password validity date.
+     *
+     * @return the maximum password validity date
+     */
+    public String getPasswordMaxValidDate() {
+        DateFormat dateFormat = new SimpleDateFormat( );
+        return Optional.ofNullable( _databaseUser.getPasswordMaxValidDate( ) ).map( dateFormat::format ).orElse( StringUtils.EMPTY );
+    }
+
+    /**
+     * Returns the maximum account validity date.
+     *
+     * @return the maximum account validity date
+     */
+    public String getAccountMaxValidDate() {
+        DateFormat dateFormat = new SimpleDateFormat( );
+        return Optional.ofNullable( _databaseUser.getAccountMaxValidDate( ) ).map( dateFormat::format ).orElse( StringUtils.EMPTY );
+    }
+
+    /**
+     * Returns the list of roles assigned to the user.
+     *
+     * @return the list of roles
+     */
+    public List<String> getRoles() {
+        return _roles;
+    }
+
+    /**
+     * Sets the list of roles assigned to the user.
+     *
+     * @param roles the list of roles to set
+     */
+    public void setRoles(List<String> roles) {
+        this._roles = roles;
+    }
+
+    /**
+     * Returns the list of groups the user belongs to.
+     *
+     * @return the list of groups
+     */
+    public List<String> getGroups() {
+        return _groups;
+    }
+
+    /**
+     * Sets the list of groups the user belongs to.
+     *
+     * @param listGroups the list of groups to set
+     */
+    public void setGroups(List<String> listGroups) {
+        this._groups = listGroups;
+    }
+
+    /**
+     * Returns the list of user attributes.
+     *
+     * @return the list of attributes
+     */
+    public List<String> getAttributes() {
+        return _attributes;
+    }
+
+    /**
+     * Sets the list of user attributes.
+     *
+     * @param attributes the list of attributes to set
+     */
+    public void setAttributes(List<String> attributes) {
+        this._attributes = attributes;
+    }
+
+}
--- src/java/fr/paris/lutece/plugins/mylutece/modules/database/authentication/service/DatabaseService.java
+++ src/java/fr/paris/lutece/plugins/mylutece/modules/database/authentication/service/DatabaseService.java
@@ -34,8 +34,6 @@
 package fr.paris.lutece.plugins.mylutece.modules.database.authentication.service;
 
 import java.sql.Timestamp;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
@@ -46,6 +44,8 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import fr.paris.lutece.plugins.mylutece.modules.database.authentication.business.DatabaseUserWrapper;
+import fr.paris.lutece.util.string.StringUtil;
 import org.apache.commons.lang3.StringUtils;
 
 import fr.paris.lutece.api.user.User;
@@ -90,7 +90,6 @@ import fr.paris.lutece.util.ReferenceItem;
 import fr.paris.lutece.util.html.HtmlTemplate;
 import fr.paris.lutece.util.password.IPasswordFactory;
 import fr.paris.lutece.util.url.UrlItem;
-import fr.paris.lutece.util.xml.XmlUtil;
 
 /**
  *
@@ -105,23 +104,7 @@ public final class DatabaseService
     // CONSTANTS
     private static final String AMPERSAND = "&";
     private static final String PLUGIN_JCAPTCHA = "jcaptcha";
-    private static final String CONSTANT_XML_USER = "user";
-    private static final String CONSTANT_XML_ACCESS_CODE = "access_code";
-    private static final String CONSTANT_XML_LAST_NAME = "last_name";
-    private static final String CONSTANT_XML_FIRST_NAME = "first_name";
-    private static final String CONSTANT_XML_EMAIL = "email";
-    private static final String CONSTANT_XML_STATUS = "status";
-    private static final String CONSTANT_XML_PASSWORD_MAX_VALID_DATE = "password_max_valid_date";
-    private static final String CONSTANT_XML_ACCOUNT_MAX_VALID_DATE = "account_max_valid_date";
-    private static final String CONSTANT_XML_ROLES = "roles";
-    private static final String CONSTANT_XML_GROUPS = "groups";
-    private static final String CONSTANT_XML_ROLE = "role";
-    private static final String CONSTANT_XML_GROUP = "group";
-    private static final String CONSTANT_XML_ATTRIBUTES = "attributes";
-    private static final String CONSTANT_XML_ATTRIBUTE = "attribute";
-    private static final String CONSTANT_XML_ATTRIBUTE_ID = "attribute-id";
-    private static final String CONSTANT_XML_ATTRIBUTE_FIELD_ID = "attribute-field-id";
-    private static final String CONSTANT_XML_ATTRIBUTE_VALUE = "attribute-value";
+    private static final String CONSTANT_ATTRIBUTE_SEPARATOR = ":";
 
     // MARKS
     private static final String MARK_SEARCH_IS_SEARCH = "search_is_search";
@@ -700,10 +683,10 @@ public final class DatabaseService
     }
 
     /**
-     * Get a XML string describing a given user
+     * Get a DatabaseUserWrapper describing a given user for export
      * 
      * @param user
-     *            The user to get the XML of.
+     *            The user.
      * @param bExportRoles
      *            True to export roles of the user, false otherwise.
      * @param bExportGroups
@@ -712,73 +695,28 @@ public final class DatabaseService
      *            True to export attributes of the user, false otherwise.
      * @param listAttributes
      *            The list of attributes to export.
-     * @param locale
-     *            The locale
-     * @return A string of XML with the information of the user.
+     * @return DatabaseUserWrapper databaseUserWrapper
      */
-    public String getXmlFromUser( DatabaseUser user, boolean bExportRoles, boolean bExportGroups, boolean bExportAttributes, List<IAttribute> listAttributes,
-            Locale locale )
+    public DatabaseUserWrapper getExportUser(DatabaseUser user, boolean bExportRoles, boolean bExportGroups, boolean bExportAttributes, List<IAttribute> listAttributes )
     {
         Plugin databasePlugin = PluginService.getPlugin( DatabasePlugin.PLUGIN_NAME );
         Plugin mylutecePlugin = PluginService.getPlugin( MyLutecePlugin.PLUGIN_NAME );
-        StringBuffer sbXml = new StringBuffer( );
-        DateFormat dateFormat = new SimpleDateFormat( );
 
-        XmlUtil.beginElement( sbXml, CONSTANT_XML_USER );
-        XmlUtil.addElement( sbXml, CONSTANT_XML_ACCESS_CODE, user.getLogin( ) );
-        XmlUtil.addElement( sbXml, CONSTANT_XML_LAST_NAME, user.getLastName( ) );
-        XmlUtil.addElement( sbXml, CONSTANT_XML_FIRST_NAME, user.getFirstName( ) );
-        XmlUtil.addElement( sbXml, CONSTANT_XML_EMAIL, user.getEmail( ) );
-        XmlUtil.addElement( sbXml, CONSTANT_XML_STATUS, Integer.toString( user.getStatus( ) ) );
-
-        String strPasswordMaxValidDate = StringUtils.EMPTY;
-
-        if ( user.getPasswordMaxValidDate( ) != null )
-        {
-            strPasswordMaxValidDate = dateFormat.format( user.getPasswordMaxValidDate( ) );
-        }
-
-        XmlUtil.addElement( sbXml, CONSTANT_XML_PASSWORD_MAX_VALID_DATE, strPasswordMaxValidDate );
-
-        String strAccountMaxValidDate = StringUtils.EMPTY;
-
-        if ( user.getAccountMaxValidDate( ) != null )
-        {
-            strAccountMaxValidDate = dateFormat.format( user.getAccountMaxValidDate( ) );
-        }
-
-        XmlUtil.addElement( sbXml, CONSTANT_XML_ACCOUNT_MAX_VALID_DATE, strAccountMaxValidDate );
+        DatabaseUserWrapper databaseUserExport = new DatabaseUserWrapper( user );
 
         if ( bExportRoles )
         {
-            List<String> listRoles = DatabaseHome.findUserRolesFromLogin( user.getLogin( ), databasePlugin );
-            XmlUtil.beginElement( sbXml, CONSTANT_XML_ROLES );
-
-            for ( String strRole : listRoles )
-            {
-                XmlUtil.addElement( sbXml, CONSTANT_XML_ROLE, strRole );
-            }
-
-            XmlUtil.endElement( sbXml, CONSTANT_XML_ROLES );
+            databaseUserExport.setRoles( DatabaseHome.findUserRolesFromLogin( user.getLogin( ), databasePlugin ) );
         }
 
         if ( bExportGroups )
         {
-            List<String> listGroups = DatabaseHome.findUserGroupsFromLogin( user.getLogin( ), databasePlugin );
-            XmlUtil.beginElement( sbXml, CONSTANT_XML_GROUPS );
-
-            for ( String strGoup : listGroups )
-            {
-                XmlUtil.addElement( sbXml, CONSTANT_XML_GROUP, strGoup );
-            }
-
-            XmlUtil.endElement( sbXml, CONSTANT_XML_GROUPS );
+            databaseUserExport.setGroups( DatabaseHome.findUserGroupsFromLogin( user.getLogin( ), databasePlugin ) );
         }
 
         if ( bExportAttributes )
         {
-            XmlUtil.beginElement( sbXml, CONSTANT_XML_ATTRIBUTES );
-
+            List<String> attributes = new ArrayList<>();
             for ( IAttribute attribute : listAttributes )
             {
                 List<MyLuteceUserField> listUserFields = MyLuteceUserFieldHome.selectUserFieldsByIdUserIdAttribute( user.getUserId( ),
@@ -786,20 +724,19 @@ public final class DatabaseService
 
                 for ( MyLuteceUserField userField : listUserFields )
                 {
-                    XmlUtil.beginElement( sbXml, CONSTANT_XML_ATTRIBUTE );
-                    XmlUtil.addElement( sbXml, CONSTANT_XML_ATTRIBUTE_ID, Integer.toString( attribute.getIdAttribute( ) ) );
-                    XmlUtil.addElement( sbXml, CONSTANT_XML_ATTRIBUTE_FIELD_ID, userField.getAttributeField( ).getIdField( ) );
-                    XmlUtil.addElement( sbXml, CONSTANT_XML_ATTRIBUTE_VALUE, userField.getValue( ) );
-                    XmlUtil.endElement( sbXml, CONSTANT_XML_ATTRIBUTE );
+                    StringBuilder strAttribute = new StringBuilder();
+                    strAttribute.append(attribute.getIdAttribute())
+                            .append(CONSTANT_ATTRIBUTE_SEPARATOR)
+                            .append(userField.getAttributeField( ).getIdField( ))
+                            .append(CONSTANT_ATTRIBUTE_SEPARATOR)
+                            .append(StringUtil.replaceAccent( userField.getValue( ) ));
+                    attributes.add(strAttribute.toString());
                 }
             }
-
-            XmlUtil.endElement( sbXml, CONSTANT_XML_ATTRIBUTES );
+            databaseUserExport.setAttributes( attributes );
         }
 
-        XmlUtil.endElement( sbXml, CONSTANT_XML_USER );
-
-        return sbXml.toString( );
+        return databaseUserExport;
     }
 
     /**
